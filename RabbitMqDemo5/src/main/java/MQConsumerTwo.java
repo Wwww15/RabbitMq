4699a9d522ff6947ef3245bdc420cbf9a4ca33eb
Wenzx <53310640+Wenzx1993@users.noreply.github.com>
2020-06-30 22:22:50 +0800

    fanout exchangge
    fanout exchange out

---
+++ RabbitMqDemo5/src/main/java/MQConsumerTwo.java
@@ -0,0 +1,52 @@
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 消费者2
+ *
+ * @Tag 扇出交换 fanout
+ */
+public class MQConsumerTwo {
+
+    public static void main(String[] args) {
+
+        try {
+            consumerMsg("fanout.exchange");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void consumerMsg(String exchange) throws IOException, TimeoutException {
+        //创建工厂连接
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        //设置连接地址
+        connectionFactory.setHost("192.168.239.128");
+        //创建连接
+        Connection connection = connectionFactory.newConnection();
+        //创建通道
+        Channel channel = connection.createChannel();
+        //声明交换机
+        channel.exchangeDeclare(exchange, BuiltinExchangeType.FANOUT);
+        //声明队列
+        String queue = channel.queueDeclare().getQueue();
+        //绑定队列
+        channel.queueBind(queue,exchange,"",null);
+        //创建消费者，消费消息
+        channel.basicConsume(queue, true, (consumerTag, message) -> {
+            //消费消息
+            String msg = new String(message.getBody(), "UTF-8");
+            System.out.println(msg);
+        }, (consumerTag) -> {
+            System.out.println(consumerTag);
+        });
+        //关闭连接
+     /*   channel.close();
+        connection.close();*/
+    }
+}
---
+++ RabbitMqDemo5/src/main/java/MQProducer.java
@@ -0,0 +1,41 @@
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 生产者
+ *
+ *  @Tag 扇出交换 fanout
+ */
+public class MQProducer {
+
+    public static void main(String[] args) {
+        try {
+            consumerMsg("fanout.exchange","扇出交换，我来啦！");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void consumerMsg(String exchange,String msg) throws IOException, TimeoutException {
+        //创建工厂连接
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        //设置连接地址
+        connectionFactory.setHost("192.168.239.128");
+        //创建连接
+        Connection connection = connectionFactory.newConnection();
+        //创建通道
+        Channel channel = connection.createChannel();
+        //声明交换机
+        channel.exchangeDeclare(exchange, BuiltinExchangeType.FANOUT);
+        //创建生产者，发送消息
+        channel.basicPublish(exchange,"",null,msg.getBytes());
+        //关闭连接
+        channel.close();
+        connection.close();
+    }
+}
